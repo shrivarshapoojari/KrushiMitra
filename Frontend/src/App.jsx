133591924c9a17ad0dbac81fc336dd466611ff1b
SHRINIDHI I <157033652+Shrinidhi-I@users.noreply.github.com>
2025-01-30 18:48:44 +0530

    new routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ImageUpload from "./components/ImageUpload"
- 
-
+import HomePage from "./components/homePage"
+import CropRecommendation from "./components/croprec"
+import CropYield from "./components/cropyield"
+import IrrigationForm from "./components/irrigation"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ImageUpload />} />
-        
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/detect-disease" element={<ImageUpload />} />
+        <Route path="/predict-yield" element={< CropYield/>} />
+        <Route path="/crop-recommendation" element={<CropRecommendation />} />
+        <Route path="/smart-irrigation" element={<IrrigationForm />} />
       </Routes>
     </BrowserRouter>
   )
